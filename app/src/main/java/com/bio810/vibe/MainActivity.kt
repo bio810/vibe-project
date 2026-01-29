8a5911ddfbf29228e4701c7479c2d1347b6dade4
bio810 <bio810@gmail.com>
2026-01-29 12:41:11 +0800

    fix: 解決資源缺失報錯並加入 UI 文字內容

--- app/src/main/java/com/bio810/vibe/MainActivity.kt
+++ app/src/main/java/com/bio810/vibe/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // 核心指令：告訴 App 顯示剛才建立的黑底紅字畫面
         setContentView(R.layout.activity_main)
     }
 }
